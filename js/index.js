07911f45216be3d766abf1bc9755edaabe9013be
cpplv <aliepins@inbox.lv>
2024-10-09 11:32:46 +0300

    Food randomly is placed on X axis

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext("2d");
 const ground = new Image();
 ground.src = "img/ground.png";
 
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
 //variables
 let refresh_rate = 100;
 let box = 32;
@@ -13,13 +16,23 @@ snake[0] = {
     x: 9 * box,
     y: 10 * box
 }
+
+let food = {
+    x: Math.floor(Math.random()*17+1)*box,
+    y: 7*box
+}
 //--------
+
+console.log("Random Number "+Math.floor(Math.random()*17+1));
+console.log(Math.floor(5.78));
 
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
 
     ctx.fillStyle = "yellow";
     ctx.fillRect(snake[0].x, snake[0].y, box, box);
+
+    ctx.drawImage(foodImg, food.x, food.y);
 }
 
 let game = setInterval(drawGame, refresh_rate);
