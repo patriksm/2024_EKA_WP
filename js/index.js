ae163bdee4a57e74491dc620635ca78fef853d18
cpplv <aliepins@inbox.lv>
2024-10-09 11:52:21 +0300

    snake as array moving

--- js/index.js
+++ js/index.js
@@ -19,20 +19,33 @@ snake[0] = {
 
 let food = {
     x: Math.floor(Math.random()*17+1)*box,
-    y: 7*box
+    y: Math.floor(Math.random()*15+3)*box
 }
 //--------
-
-console.log("Random Number "+Math.floor(Math.random()*17+1));
-console.log(Math.floor(5.78));
 
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
 
-    ctx.fillStyle = "yellow";
-    ctx.fillRect(snake[0].x, snake[0].y, box, box);
+    for(let i = 0; i < snake.length; i++){
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 
     ctx.drawImage(foodImg, food.x, food.y);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    snake.pop();
+
+    snakeX += box;
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
 
 let game = setInterval(drawGame, refresh_rate);
