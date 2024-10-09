99e99c9b2aa5d48babde78db69370ae40a077640
cpplv <aliepins@inbox.lv>
2024-10-09 10:49:16 +0300

    image drawn and screen refresh introduced

--- js/index.js
+++ js/index.js
@@ -4,4 +4,12 @@ const ctx = canvas.getContext("2d");
 const ground = new Image();
 ground.src = "img/ground.png";
 
-ctx.drawImage(ground, 0, 0);
+//variables
+let refresh_rate = 100;
+//--------
+
+function drawGame(){
+    ctx.drawImage(ground, 0, 0);
+}
+
+let game = setInterval(drawGame, refresh_rate);
