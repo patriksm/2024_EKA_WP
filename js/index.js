e7018393ee0632a8aae91aa4fd2d2a56d47f6c96
cpplv <aliepins@inbox.lv>
2024-10-09 10:29:29 +0300

    ground drawn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-// ctx.fillRect(20, 20, 150, 100);
+const ground = new Image();
+ground.src = "img/ground.png";
 
-ctx.beginPath();
-ctx.moveTo(20, 20);
-ctx.lineTo(40, 100);
-ctx.lineTo(70, 100);
-ctx.stroke();
+ctx.drawImage(ground, 0, 0);
